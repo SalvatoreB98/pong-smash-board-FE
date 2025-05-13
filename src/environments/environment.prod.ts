ac81a40a923950db4aedf1c5a63f089ceccd209a
Salvatore Butera <z.salvatore.butera@guest.rcs.it>
2025-05-13 22:35:40 +0200

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   mock: false,
-  production: true,
-  apiUrl: 'https://pong-smash-board-be.vercel.app/',
+  production: false,
+  apiUrl: 'https://pong-smash-board-be.vercel.app',
   supabase: {
     url: "https://fhkmvhtwavswlhnroans.supabase.co",
     ANON: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoa212aHR3YXZzd2xobnJvYW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxNDM5NjAsImV4cCI6MjA1NDcxOTk2MH0.ctwlPWoSHgBtcaWOSraGmI4_fILq2eBOFyDR2_nz4Z4"
--- src/services/supabase-auth.service.ts
+++ src/services/supabase-auth.service.ts
@@ -15,36 +15,36 @@ export class SupabaseAuthService {
     );
   }
 
-  /** Sign Up User */
   async signUp(email: string, password: string) {
     return this.supabase.auth.signUp({ email, password });
   }
 
-  /** Login User */
   async signIn(email: string, password: string) {
     return this.supabase.auth.signInWithPassword({ email, password });
   }
 
-  /** Logout User */
   async signOut() {
     return this.supabase.auth.signOut();
   }
 
-  /** Get Current User */
   async getUser() {
     return this.supabase.auth.getUser();
   }
 
-  /** Refresh Token (Handled Automatically by Supabase) */
   async refreshSession() {
     return this.supabase.auth.refreshSession();
   }
-  /** Sign in with Google */
   async signInWithGoogle() {
+    const isLocalhost = window.location.hostname === 'localhost';
+
+    const redirectUrl = isLocalhost
+      ? 'http://localhost:3000/callback'
+      : environment.apiUrl + '/callback';
+
     return this.supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${environment.apiUrl}/callback`,
+        redirectTo: redirectUrl,
       },
     });
   }
