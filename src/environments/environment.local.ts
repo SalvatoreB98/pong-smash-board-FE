00ee10e08c4b9f09a173a66c8f2a74727b603d26
Salvatore Butera <z.salvatore.butera@guest.rcs.it>
2025-08-23 12:47:25 +0200

    Update work in progress

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  mock: false,
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  supabase: {
+    url: "https://fhkmvhtwavswlhnroans.supabase.co",
+    ANON: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoa212aHR3YXZzd2xobnJvYW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxNDM5NjAsImV4cCI6MjA1NDcxOTk2MH0.ctwlPWoSHgBtcaWOSraGmI4_fILq2eBOFyDR2_nz4Z4"
+  },
+  firebase: {
+    apiKey: 'AIzaSyD7g5FXuYLkkifvO_jju8OsISU5BKPK5g4',
+    authDomain: 'ping-pong-firebase-37f28.firebaseapp.com',
+    projectId: 'ping-pong-firebase-37f28',
+    storageBucket: 'ping-pong-firebase-37f28.firebasestorage.app',
+  }
+};
