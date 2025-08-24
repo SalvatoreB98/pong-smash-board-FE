204fbe2b31ac0b4d7b57d8fff143fba9f094bd90
Salvatore Butera <z.salvatore.butera@guest.rcs.it>
2025-08-25 00:14:28 +0200

    competition first steps

---
+++ src/app/profile/complete-profile/competition-start/competition-start.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslatePipe } from '../../../utils/translate.pipe';
+
+@Component({
+  selector: 'app-competition-start',
+  imports: [TranslatePipe],
+  templateUrl: './competition-start.component.html',
+  styleUrl: './competition-start.component.scss'
+})
+export class CompetitionStartComponent {
+
+}
