2dcf1ac87797dafbd4b8f76c86b8f6435f63693d
Salvatore Butera <z.salvatore.butera@guest.rcs.it>
2025-08-24 15:31:35 +0200

    userState

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,16 +1,25 @@
 // services/user.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { IUserState } from './interfaces/Interfaces';
 import { API_PATHS } from '../api/api.config';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
+
+  private userState: IUserState | {} = {};
+
   constructor(private http: HttpClient) { }
 
   getUserState(): Observable<IUserState | {}> {
-    return this.http.get<IUserState | {}>(API_PATHS.userState);
+    if (Object.keys(this.userState).length > 0) {
+      return of(this.userState);
+    } else {
+      return this.http.get<IUserState | {}>(API_PATHS.userState).pipe(
+        tap((state) => this.userState = state)
+      );
+    }
   }
-
 }
