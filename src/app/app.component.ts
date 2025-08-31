5d4ff3034f4815cd1ae9983e20a80f670357f444
Salvatore Butera <z.salvatore.butera@guest.rcs.it>
2025-08-31 11:35:04 +0200

    Update importante

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoaderComponent } from './utils/components/loader/loader.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { DebugOverlayComponent } from '../debug-overlay/debug-overlay.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss',
-  imports: [LoaderComponent, RouterOutlet, NavbarComponent]
+  imports: [LoaderComponent, RouterOutlet, NavbarComponent, DebugOverlayComponent]
 })
 export class AppComponent {
   title = 'pong-smash-board';
---
+++ src/debug-overlay/debug-overlay.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import { CompetitionService } from '../services/competitions.service';
+import { DataService } from '../services/data.service';
+import { UserService } from '../services/user.service';
+import { AuthService } from '../services/auth.service';
+import { SHARED_IMPORTS } from '../app/common/imports/shared.imports';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-debug-overlay',
+  imports: [...SHARED_IMPORTS, CommonModule],
+  templateUrl: './debug-overlay.component.html',
+  styleUrl: './debug-overlay.component.scss'
+})
+export class DebugOverlayComponent {
+  private comp = inject(CompetitionService);
+  private user = inject(UserService);
+  private auth = inject(AuthService, { optional: true });
+
+  // stream principali
+  list$ = this.comp.list$;
+  active$ = this.comp.activeCompetition$;
+  userState$ = this.user.userState$();         // suppongo ritorni observable
+  isLoggedIn$ = this.auth?.isLoggedIn$ ?? null;
+
+  isVisible = true;
+
+  close() {
+    this.isVisible = false;
+  }
+  open() {
+    this.isVisible = true;
+  }
+}
