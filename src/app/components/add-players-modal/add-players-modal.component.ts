3ba2fa527843d7bb6a03629904287e47d5245ea8
Salvatore Butera <z.salvatore.butera@guest.rcs.it>
2025-09-06 13:19:48 +0200

    Add players fix

--- src/app/components/add-players-modal/add-players-modal.component.ts
+++ src/app/components/add-players-modal/add-players-modal.component.ts
@@ -2,19 +2,47 @@ import { Component, inject } from '@angular/core';
 import { SHARED_IMPORTS } from '../../common/imports/shared.imports';
 import { UserService } from '../../../services/user.service';
 import { CompetitionService } from '../../../services/competitions.service';
-
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+export interface IPlayerToAdd {
+  name: string;
+  surname: string;
+  imgUrl: string;
+  nickname: string;
+}
 @Component({
   selector: 'app-add-players-modal',
-  imports: [...SHARED_IMPORTS],
+  imports: [...SHARED_IMPORTS, FormsModule, ReactiveFormsModule],
   templateUrl: './add-players-modal.component.html',
   styleUrl: './add-players-modal.component.scss'
 })
 export class AddPlayersModalComponent {
 
+  addPlayerForm: FormGroup ;
   competitionService = inject(CompetitionService);
   activeCompetition$ = this.competitionService.activeCompetition$;
-  constructor() { }
+  playersToAdd: IPlayerToAdd[] = [];
+
+  constructor(private fb: FormBuilder) {
+    this.addPlayerForm = this.fb.group({
+      name: [''],
+      surname: [''],
+      imgUrl: [''],
+      nickname: ['']
+    });
+  }
+
   addPlayer() {
-    // Logica per aggiungere un giocatore
+    if (this.addPlayerForm.valid) {
+      const player: IPlayerToAdd = this.addPlayerForm.value;
+      this.playersToAdd.push(player);
+      this.addPlayerForm.reset();
+    }
+  }
+  removePlayer(index: number) {
+    this.playersToAdd.splice(index, 1);
+  }
+
+  addPlayers() {
+    // Logica per aggiungere i giocatori
   }
 }
