b0c36923dfa68cee44a5ac44775dd81c120ecf63
Salvatore Butera <z.salvatore.butera@guest.rcs.it>
2025-09-13 00:26:56 +0200

    OK

--- src/app/components/add-match-modal/manual-points/manual-points.component.ts
+++ src/app/components/add-match-modal/manual-points/manual-points.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, inject, Input, Output } from '@angular/core';
 import { SHARED_IMPORTS } from '../../../common/imports/shared.imports';
 import { IPlayer } from '../../../../services/players.service';
+import { CompetitionService } from '../../../../services/competitions.service';
+import { ICompetition } from '../../../../api/competition.api';
 
 @Component({
   selector: 'app-manual-points',
@@ -12,20 +14,27 @@ export class ManualPointsComponent {
   @Output() close = new EventEmitter<any>();
   isMobile = false;
   @Input() maxSets = 5;
-  @Input() maxPoints = 11;
+  @Input() maxPoints = 21;
   @Input() player1: IPlayer | null = null;
+  competitionService = inject(CompetitionService);
+  competition: ICompetition | null = null;
 
   ngOnChanges() {
     console.info('player1 value:', this.player1);
     console.info('player2 value:', this.player2);
   }
-  
+
   @Input() player2: IPlayer | null = null;
   player1Points = 0;
   player2Points = 0;
 
   ngOnInit() {
     this.checkViewport();
+    this.competitionService.activeCompetition$.subscribe(comp => {
+      this.competition = comp;
+      this.maxPoints = this.competition?.['points_type'] || 21;
+      this.maxSets = this.competition?.['sets_type'] || 10;
+    });
   }
 
   @HostListener('window:resize')
