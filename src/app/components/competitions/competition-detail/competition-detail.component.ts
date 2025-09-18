42c1f025891567387a9e311a6adba944340f854d
Salvatore Butera <salvatore.butera1998@gmail.com>
2025-09-18 12:55:37 +0200

    Add competition actions for favorite, details, and edit

--- src/app/components/competitions/competition-detail/competition-detail.component.ts
+++ src/app/components/competitions/competition-detail/competition-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { SHARED_IMPORTS } from '../../../common/imports/shared.imports';
 import { ICompetition } from '../../../../api/competition.api';
 import { ModalService } from '../../../../services/modal.service';
+import { CompetitionService } from '../../../../services/competitions.service';
 
 @Component({
   selector: 'app-competition-detail',
@@ -12,6 +13,9 @@ import { ModalService } from '../../../../services/modal.service';
 export class CompetitionDetailComponent {
   @Input() competition: ICompetition | null = null;
   @Output() actionSelected = new EventEmitter<{ action: string, competition: ICompetition | null }>();
+  private competitionService = inject(CompetitionService);
+  readonly detailsModalName = 'competitionDetailsModal';
+  readonly editModalName = 'editCompetitionModal';
   constructor(public modalService: ModalService) { }
 
   ngOnInit() {
@@ -22,6 +26,27 @@ export class CompetitionDetailComponent {
     return !array || (Array.isArray(array) && array.length === 0);
   }
   onDropdownAction(action: string) {
+    if (!this.competition?.id) {
+      return;
+    }
+
+    switch (action) {
+      case 'favorite':
+        this.competitionService.updateActiveCompetition(this.competition.id).subscribe();
+        break;
+      case 'delete':
+        this.competitionService.remove(this.competition.id).subscribe(() => {
+          this.competitionService.getCompetitions(true);
+        });
+        break;
+      case 'details':
+        this.modalService.openModal(this.detailsModalName);
+        break;
+      case 'edit':
+        this.modalService.openModal(this.editModalName);
+        break;
+    }
+
     this.actionSelected.emit({ action, competition: this.competition });
   }
 }
