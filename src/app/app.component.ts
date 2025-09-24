89e5e3429401a11a2f52cda1e57245569fedf29e
Salvatore Butera <z.salvatore.butera@guest.rcs.it>
2025-09-25 01:15:44 +0200

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { LoaderComponent } from './utils/components/loader/loader.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { DebugOverlayComponent } from '../debug-overlay/debug-overlay.component';
+import { SHARED_IMPORTS } from './common/imports/shared.imports';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss',
-  imports: [LoaderComponent, RouterOutlet, NavbarComponent, DebugOverlayComponent]
+  imports: [LoaderComponent, RouterOutlet, NavbarComponent, DebugOverlayComponent, SHARED_IMPORTS]
 })
 export class AppComponent {
   title = 'pong-smash-board';
