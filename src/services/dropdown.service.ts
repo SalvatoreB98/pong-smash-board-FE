5e0de266f76eccd5c1d2fbfa1d05e5e74009af0c
Salvatore Butera <z.salvatore.butera@guest.rcs.it>
2025-09-28 01:21:59 +0200

    Acceptable

--- src/services/dropdown.service.ts
+++ src/services/dropdown.service.ts
@@ -17,19 +17,31 @@ interface DropdownState {
 export class DropdownService {
   private stateSubject = new BehaviorSubject<DropdownState | null>(null);
   readonly state$ = this.stateSubject.asObservable();
-
+  static isOpen = false;
   private actionSubject = new Subject<string>();
   readonly action$ = this.actionSubject.asObservable();
 
   open(actions: DropdownAction[], anchor: HTMLElement) {
+    const currentState = this.stateSubject.getValue();
+    if (DropdownService.isOpen) {
+      // Se l'anchor è diverso, apri semplicemente il nuovo menu senza chiudere prima
+      if (currentState && currentState.anchor !== anchor) {
+        this.stateSubject.next({ actions, anchor });
+        return;
+      }
+      this.close();
+      return;
+    }
     if (!anchor) {
       return;
     }
     this.stateSubject.next({ actions, anchor });
+    DropdownService.isOpen = true;
   }
 
   close() {
     this.stateSubject.next(null);
+    DropdownService.isOpen = false;
   }
 
   emit(value: string) {
